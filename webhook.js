bf9a73c8af5330046b5a85442519f652827249a7
reachviji <reachvijiarun@gmail.com>
2017-09-21 14:23:17 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,5 +1,5 @@
 'use strict';
-const WebSocket = require('ws');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,22 +9,17 @@ restService.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// call the other js file
-//const websocket = require('./websocket');
-
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-	
-    //websocket.hello(speech);
-	
     return res.json({
         speech: speech,
         displayText: speech,
         source: 'webhook-echo-sample'
     });
 });
+
 
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
