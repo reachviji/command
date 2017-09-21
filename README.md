b8aa6935503931730c24904c0dd51e2cfb8a2950
reachviji <reachvijiarun@gmail.com>
2017-09-21 14:53:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Webhook-and-API.AI-Sample - Echo your text
+
+Whatever user asks Google Assistant will be sent to your API and then same text will be sent back as speech text.
+
+This simple app will help you understand
+- How to write an API in node
+- How to integrate your node JS app with API.AI as webhook
+
+# Deploy to:
+ Can be deployed in Heroku, or ngrok, but need to check for alternatives
+
+# How it works?
+- Push this app to Heroku/ngrok
+- use www.hurl.it to test it
---
+++ webhook.js
@@ -0,0 +1,26 @@
+'use strict';
+const WebSocket = require('ws');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const restService = express();
+
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+restService.use(bodyParser.json());
+
+restService.post('/echo', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.command ? req.body.result.parameters.command : "Seems like some problem. Speak again."
+	
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'command'
+    });
+});
+
+restService.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
