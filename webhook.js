5898be0ec75478f844f52ff36f6d652423be0622
reachviji <reachvijiarun@gmail.com>
2017-09-21 14:50:34 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -12,7 +12,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/command', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.command ? req.body.result.parameters.command : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
