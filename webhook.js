5a262e5d15a6d6fb804a79246e744f232b08fbe2
reachviji <reachvijiarun@gmail.com>
2017-09-22 12:23:04 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,38 +1,62 @@
 'use strict';
-const Restify = require('restify');
-const server = Restify.createServer({
-  name: "command"
-});
-const request = require('request');
-const PORT = process.env.PORT || 8000;
 
-// call the other js file
-const websocket = require('./websocket');
+var WebSocket = require('ws');
+const express = require('express');
+const bodyParser = require('body-parser');
+const restService = express();
+restService.use(bodyParser.urlencoded({
+	extended: true
+}));
 
+restService.use(bodyParser.json());
 
-server.use(Restify.bodyParser());
-server.use(Restify.jsonp());
-
-// POST route handler
-server.post('/command', (req, res, next) => {''
-  let {
-    status,
-    result
-  } = req.body;
-
-   if(status.code === 200){
-	 const command = result.parameters;
-	 let speech = `Robot will ` +result.parameters.command + ` now`;
-	 // call the websocket and see for the response
-	 console.log("This is from webhook: " +speech);
-	 
-	 res.json({
+restService.post('/echo', function(req, res) {
+	var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.command ? req.body.result.parameters.command : "Seems like some problem. Speak again."
+	
+	//senddatatows(speech);
+	console.log("This is from webhook: " +speech);
+	senddatatows(speech);
+	return res.json({
 		speech: speech,
-        displayText: speech,
-        source: "speechapi" 
-	  }); 
-	websocket.hello(speech);
-   }  
+		displayText: speech,
+		source: 'command'
+	});
+//	return speech;
+	
 });
 
-server.listen(PORT, () => console.log(`Robot Command running on ${PORT}`));
+
+function senddatatows(speech) {
+	// websocket link
+	var ws = new WebSocket('wss://ws-broadcast-server-d062507.cfapps.eu10.hana.ondemand.com');
+//var speech = senddatatoapi();
+	ws.on('open', function open() {
+		ws.send(speech);
+	});
+	ws.on('message', function incoming(msg) {
+		console.log("This is from websocket: " +msg);
+		var value = {
+			"source":"command",
+			"destination": "/chatbot",
+			"data": msg
+		}
+		try{
+			var data = JSON.parse(JSON.stringify(value));
+			if(data.source && data.destination && data.data){
+				console.log("receive data from: "+data.data);
+			}
+		}	
+		catch(e){
+			console.error(e);
+		}	
+	});
+	ws.on('close', function close() {
+  //ws_client_state = "offline";
+		console.log('websocket cloud client disconnected');
+	});
+}
+
+
+restService.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
