d62d4fb3f24ad34c6f12f157e725acd162fa4b10
reachviji <reachvijiarun@gmail.com>
2017-09-22 12:48:54 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -4,6 +4,7 @@ var WebSocket = require('ws');
 const express = require('express');
 const bodyParser = require('body-parser');
 const restService = express();
+
 restService.use(bodyParser.urlencoded({
 	extended: true
 }));
@@ -12,8 +13,6 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
 	var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.command ? req.body.result.parameters.command : "Seems like some problem. Speak again."
-	
-	//senddatatows(speech);
 	console.log("This is from webhook: " +speech);
 	return res.json({
 		speech: speech,
@@ -22,32 +21,3 @@ restService.post('/echo', function(req, res) {
 	});
 	
 });
-
-
-function senddatatows(speech) {
-	// websocket link
-	var ws = new WebSocket('wss://ws-broadcast-server-d062507.cfapps.eu10.hana.ondemand.com');
-	ws.on('open', function open() {
-		ws.send(speech);
-	});
-	ws.on('message', function incoming(msg) {
-		console.log("This is from websocket: " +msg);
-		var value = {
-			"source":"command",
-			"destination": "/chatbot",
-			"data": msg
-		}
-		try{
-			var data = JSON.parse(JSON.stringify(value));
-			if(data.source && data.destination && data.data){
-				console.log("receive data from: "+data.data);
-			}
-		}	
-		catch(e){
-			console.error(e);
-		}	
-	});
-	ws.on('close', function close() {
-		console.log('websocket cloud client disconnected');
-	});
-}
